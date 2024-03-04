169ee72334e62b3cc439ca3bf229d48f35775543
Roberto R. Expósito <roberto.rey.exposito@udc.es>
2024-03-04 10:19:39 +0100

    Update for 2023/2024

--- ej3/provisioning/dockerfiles/webapp/src/app.py
+++ ej3/provisioning/dockerfiles/webapp/src/app.py
@@ -1,15 +1,15 @@
 from flask import Flask
-from flask import Markup
 from flask import render_template
+from markupsafe import Markup
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine,text
 
 app = Flask(__name__)
 
 # Modify db_host for configuring MySQL connection
-db_host = 'xxx-aisi2223-db'
+db_host = 'xxx2324-db'
 # URI format: mysql://username:password@server:port/db_name
-db_uri = 'mysql://flask:flask@%s/database' % (db_host)
+db_uri = 'mysql://flask:12345@%s:3306/mydb' % (db_host)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -18,7 +18,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 def test():
     error = None
     mysql_result = False
-
+    
     try:
         engine = create_engine(db_uri)
         query = text('SELECT 1')
@@ -33,9 +33,11 @@ def test():
         pass
 
     if mysql_result:
-        result = Markup('<span style="color: green;">PASSED</span>')
+        height = '390px'
+        result = Markup('<span style="color: green;">OK</span>')
     else:
+        height = '440px'
         result = Markup('<span style="color: red;">FAILED</span><p><span style="color: red;">{}</span>').format(error)
 
-    # Return the page with the result.
-    return render_template('index.html', result=result,db_uri=db_uri)
+    # Return the page with the result
+    return render_template('index.html', result=result, db_uri=db_uri, height=height)
